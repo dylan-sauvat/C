0b80955989e4c0a5eff4a4163cbf470e89dd396d
dylan-sauvat <121093882+dylan-sauvat@users.noreply.github.com>
2023-03-13 15:54:03 +0100

    Add files via upload

---
+++ Jour1/Job03/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  int a = 10, b = 2;
+  int division = a / b;
+  int addition = a + b;
+  int soustraction = a - b;
+  int multiplication = a * b;
+
+  printf("Résultat de la division : %d\n", division);
+  printf("Résultat de l'addition : %d\n", addition);
+  printf("Résultat de la soustraction : %d\n", soustraction);
+  printf("Résultat de la multiplication : %d\n", multiplication);
+
+  return 0;
+}
